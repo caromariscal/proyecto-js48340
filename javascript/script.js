f0beb06dbd61f0d2dc9f2cd57f43c2271cee6906
caro <cmariscal2001@gmail.com>
2023-02-12 17:33:30 -0300

    agrego api

--- javascript/script.js
+++ javascript/script.js
@@ -4,6 +4,7 @@ import { register as elegirRol } from './elegir-rol.js';
 // import { register as mostrarPersonaje } from './mostrar-personaje.js';
 import { register as ingresarItems } from './ingresar-items.js';
 import { register as revisarItems } from './revisar-items.js';
+import { register as galletaFortuna } from './fortuna.js';
 
 registerIngresarNombre('[data-key="1"]');
 revisarNombre('[data-key="2"]');
@@ -11,10 +12,11 @@ elegirRol('[data-key="3"]');
 // mostrarPersonaje('[data-key="4"]');
 ingresarItems('[data-key="5"]');
 revisarItems('[data-key="6"]');
+galletaFortuna('[data-key="7"]');
 
 
 document.addEventListener('keydown', (e) => {
-    if(/^Digit[1-6]/g.test(e.code)){
+    if(/^Digit[1-7]/g.test(e.code)){
         const key = e.code.replace('Digit', '');
         const action = document.querySelector(`[data-key="${key}"]`);
         if(action){
